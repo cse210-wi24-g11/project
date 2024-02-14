e8d93c89ab2a988e2c74da2a3fff322382e08b07
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-02-14 12:33:47 -0800

    build(lint): add type aware lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,9 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
@@ -16,4 +18,10 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  }
 }
