b2170ce6b98db9e47f0886a35f890e8cb4e0e81f
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-02-14 21:09:07 -0800

    test: add dummy file with test file for ci

---
+++ src/foo.ts
@@ -0,0 +1,8 @@
+/**
+ * @overview
+ * this file exists to have a sample file with associated tests so we don't get ci errors about no tests existing
+ */
+
+export function add(a: number, b: number): number {
+  return a + b
+}
