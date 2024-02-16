11ee3a82f9af45b0dc4e4df50bc25cf62a6db2cc
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-02-16 11:44:39 -0800

    build: set up tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'postcss.config.js', 'tailwind.config.js'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
