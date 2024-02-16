e578b09f62eb38f604cb2d1520de9a271ca30c24
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-02-16 11:35:18 -0800

    refactor: set up initial file structure

---
+++ src/components/button/button.tsx
@@ -0,0 +1,8 @@
+/**
+ * @overview
+ * this file exists to have a sample component.
+ */
+
+export function Button() {
+  return <button />
+}
--- tests/utils/foo.test.ts
+++ tests/utils/foo.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { add } from '../src/foo.ts'
+import { add } from '../../src/utils/foo.ts'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3)
