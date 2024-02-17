3f570953084084c6a883d5d4832a403b3fe71884
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-02-16 16:20:45 -0800

    style: run linter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/app/app.tsx'
+
+import App from '@/pages/app/app.tsx'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- tests/utils/foo.test.ts
+++ tests/utils/foo.test.ts
@@ -1,4 +1,5 @@
 import { expect, test } from 'vitest'
+
 import { add } from '../../src/utils/foo.ts'
 
 test('adds 1 + 2 to equal 3', () => {
