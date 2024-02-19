e89eaae0a01f43f9a1cba76f0033da4ca68dff33
Jiaqi Gu <46491224+JG1109@users.noreply.github.com>
2024-02-19 11:45:34 -0800

    build: navigation bar for summary pages

---
+++ src/components/navigation/summaryBar.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+export function SummaryBar() {
+  const [selectedButton, setSelectedButton] = useState<string>('Day')
+  const buttonStyle = (button: string) => {
+    if (button === selectedButton) {
+      return 'rounded-3xl border w-1/3 bg-blue-500 text-white hover:border-blue-500'
+    } else {
+      return 'rounded-3xl border w-1/3 bg-white hover:bg-gray-200 text-slate-500 hover:border-gray-200'
+    }
+  }
+
+  return (
+    <div className="w-full bg-white">
+      <div className="fixed left-0 top-0 w-full flex-row rounded-3xl border bg-white">
+        <button
+          className={buttonStyle('Day')}
+          onClick={() => setSelectedButton('Day')}
+        >
+          Day
+        </button>
+        <button
+          className={buttonStyle('Week')}
+          onClick={() => setSelectedButton('Week')}
+        >
+          Week
+        </button>
+        <button
+          className={buttonStyle('Month')}
+          onClick={() => setSelectedButton('Month')}
+        >
+          Month
+        </button>
+      </div>
+    </div>
+  )
+}
