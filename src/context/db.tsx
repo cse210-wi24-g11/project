18f5ad1c8d05a6fe4c3cc87caa6fc8d92afc5012
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-03-01 12:15:33 -0800

    chore: resolve lint warning

--- src/context/db.tsx
+++ src/context/db.tsx
@@ -14,6 +14,10 @@ type Db = globalThis.IDBDatabase | null
 
 const IndexeddbContext = createContext<Undefinable<Db>>(undefined)
 
+// disable linting here for react fast refresh - we won't really be changing the provider anyway,
+// so no fast refresh here really shouldn't be an issue.
+// and we want to keep them in the same file so that `IndexeddbContext` can only be accessed via our exported functions here
+// eslint-disable-next-line react-refresh/only-export-components
 export function useDb() {
   const data = useContext(IndexeddbContext)
   if (data === undefined) {
