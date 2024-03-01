c715473257dd06e6b526a87f8a2a49c79fed9f7d
Jiaqi Gu <46491224+JG1109@users.noreply.github.com>
2024-03-01 11:51:47 -0800

    Update day-summary.tsx

--- src/pages/day-summary.tsx
+++ src/pages/day-summary.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 import { MainNavBar } from '@/components/navigation/main-navbar.tsx'
 import { SummaryBar } from '@/components/navigation/summary-bar.tsx'
 
-export function DaySummary({db: IDBDatabase}) {
+export function DaySummary({ db: IDBDatabase }) {
   return (
     <>
       <SummaryBar />
@@ -17,7 +17,7 @@ export function DaySummary({db: IDBDatabase}) {
         </div>
         <NavLink className="mt-5 w-80 bg-white" to="/UpdateMood">
           <div className="rounded-md border px-1 py-2">
-            <div className="flex items-center justify-start">
+            <div className="fßex items-center justify-start">
               <div className="mr-2 h-10 w-10 rounded-md border border-gray-200">
                 {/* insert mood source (img?) here */}
                 <div></div>
