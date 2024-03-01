840b22d25894aea8f4347ded3cb55249856aaf8c
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-03-01 11:52:04 -0800

    refactor: move db to context

---
+++ src/context/db.tsx
@@ -0,0 +1,44 @@
+import { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react'
+
+import { openDb } from '../utils/db.ts'
+
+type Undefinable<T> = T | undefined
+
+type Db = globalThis.IDBDatabase | null
+
+const IndexeddbContext = createContext<Undefinable<Db>>(undefined)
+
+export function useDb() {
+  const data = useContext(IndexeddbContext)
+  if (data === undefined) {
+    throw new Error('useDb must be used in a DbProvider')
+  }
+  return data
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type DbProviderProps = PropsWithChildren<{}>
+
+export function DbProvider({ children }: DbProviderProps) {
+  const [db, setDb] = useState<Db>(null)
+
+  useEffect(() => {
+    openDb()
+      .then((db) => {
+        // create a global reference to the database
+        // this will allow us to access the database from anywhere in the app
+        // without having to pass the database object around
+        setDb(db)
+        console.log('launch: database opened successfully')
+      })
+      .catch((error) => {
+        console.error('Failed to open database:', error)
+      })
+  }, [])
+
+  return (
+    <IndexeddbContext.Provider value={db}>
+      {children}
+    </IndexeddbContext.Provider>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { defaultTheme, Provider } from '@adobe/react-spectrum'
 
-import App from './app.tsx'
+import { DbProvider } from './context/db.tsx'
+import { DaySummary } from './pages/day-summary.tsx'
+import { UpdateMood } from './pages/update-mood.tsx'
 
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -14,7 +18,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       // we'll just use the default theme for now, can customize later
       theme={defaultTheme}
     >
-      <App />
+      <DbProvider>
+        <Router>
+          <Routes>
+            <Route path="/DaySummary" element={<DaySummary />} />
+            <Route path="/UpdateMood" element={<UpdateMood />} />
+          </Routes>
+        </Router>
+      </DbProvider>
     </Provider>
   </React.StrictMode>,
 )
