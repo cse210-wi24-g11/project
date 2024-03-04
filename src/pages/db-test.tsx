8dbcb64c667923b6cbfcd4f620000b1f2f5d2c0e
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-03-04 11:46:46 -0800

    fix: update useDb uses to match new api

--- src/pages/db-test.tsx
+++ src/pages/db-test.tsx
@@ -3,18 +3,18 @@ import { useEffect } from 'react'
 import { useDb } from '@/context/db.tsx'
 
 export function DbTest() {
-  const db = useDb()
+  const { getDb } = useDb()
 
   useEffect(() => {
-    if (db) {
-      console.log('success: db connection is established')
+    async function run() {
+      const db = await getDb()
       db.transaction('mood', 'readwrite')
         .objectStore('mood')
         .put({ id: '1', color: 'blue', image: new Blob() })
-    } else {
-      console.log('error: db is still null')
     }
-  }, [db])
+
+    void run()
+  }, [getDb])
 
   return (
     <div>
