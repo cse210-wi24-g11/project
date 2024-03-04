329913253fb234d7c5fd88fddc1297e45ca5baf1
Jiaqi Gu <46491224+JG1109@users.noreply.github.com>
2024-03-04 14:55:25 -0800

    refactor: further improve the styling

--- src/components/navigation/summary-bar.tsx
+++ src/components/navigation/summary-bar.tsx
@@ -7,18 +7,19 @@ type SummaryBarProps = {
 }
 
 export function SummaryBar({ summaryNavBarItem }: SummaryBarProps) {
-  const buttonGeneralStyle = 'mx-8 text-lg pb-1 px-2 bg-white rounded-none mb-[-2px]'
+  const buttonGeneralStyle =
+    'text-lg py-1 px-2 rounded-none mb-[-4px] flex-grow'
   const buttonSelectStyle = (button: SummaryNavbarItem) => {
     if (button === summaryNavBarItem) {
-      return `${buttonGeneralStyle} blue-500 text-blue-500 border-b-4 border-b-blue-500`
+      return `${buttonGeneralStyle} bg-blue-100 text-blue-500 border-b-4 border-b-blue-500`
     } else {
-      return `${buttonGeneralStyle} slate-500 text-slate-500`
+      return `${buttonGeneralStyle} bg-white slate-500 text-slate-500`
     }
   }
 
   return (
     <div className="w-full bg-white">
-      <div className="fixed left-0 top-10 w-full flex-row border-b-2 bg-white px-2 pb-1">
+      <div className="fixed left-0 top-10 flex w-full border-b-2 bg-white pb-1">
         <Link className={buttonSelectStyle('Day')} to="/DaySummary">
           Day
         </Link>
