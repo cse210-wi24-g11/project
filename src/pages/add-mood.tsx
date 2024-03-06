7c717c8ffe5f253002e0ecccf2dec9fb06c3ff18
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-03-05 21:41:51 -0800

    feat(add mood page): mood selection and submission

--- src/pages/add-mood.tsx
+++ src/pages/add-mood.tsx
@@ -1,19 +1,174 @@
-import { Navbar } from '@/components/navbar/navbar.tsx'
+import { useEffect, useMemo, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Button } from '@react-spectrum/button'
+import { TextField } from '@react-spectrum/textfield'
+import Send from '@spectrum-icons/workflow/Send'
+import More from '@spectrum-icons/workflow/More'
+
+import {
+  ADD_MOOD_ROUTE,
+  DAY_SUMMARY_ROUTE,
+  MOOD_COLLECTION_ROUTE,
+} from '@/routes.ts'
+import { DbRecord, getFavoriteMoods, putEntry } from '@/utils/db.ts'
+
+import { useDb } from '@/context/db.tsx'
+import { MainNavBar } from '@/components/navigation/main-navbar.tsx'
+import { MoodSwatch } from '@/components/mood-swatch/mood-swatch.tsx'
+
+const MOCK_FAVORITES = [
+  { id: 'sdfa;sdf', color: '#ff0000', imagePath: '/vite.svg ' },
+]
+
+type State = {
+  selectedMood: DbRecord<'mood'>
+}
 
 export function AddMood() {
+  const state = useLocationState()
+  const navigate = useNavigate()
+  const { getDb } = useDb()
+
+  const [mood, setMood] = useState<DbRecord<'mood'> | null>(() =>
+    state === null ? null : state.selectedMood,
+  )
+  const [description, setDescription] = useState('')
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const [favoriteMoods, setFavoriteMoods] = useState<DbRecord<'mood'>[]>([])
+  useEffect(() => {
+    async function loadFavoriteMoods() {
+      const db = await getDb()
+      const favoriteMoods = await getFavoriteMoods(db)
+
+      setFavoriteMoods(favoriteMoods?.length ? favoriteMoods : MOCK_FAVORITES)
+    }
+    void loadFavoriteMoods()
+  }, [getDb])
+
+  function pickFromMoodCollection() {
+    navigate(MOOD_COLLECTION_ROUTE, {
+      state: {
+        returnTo: ADD_MOOD_ROUTE,
+      },
+    })
+  }
+
+  async function addMoodEntry() {
+    // sanity check
+    if (mood === null) {
+      return
+    }
+
+    setIsSubmitting(true)
+
+    const entry: DbRecord<'entry'> = {
+      id: window.crypto.randomUUID(),
+      moodId: mood.id,
+      description,
+      timestamp: new Date(),
+    }
+
+    const db = await getDb()
+    await putEntry(db, entry)
+    setIsSubmitting(false)
+
+    navigate(DAY_SUMMARY_ROUTE)
+  }
+
   return (
     <>
-      <main>
-        <DayPreview />
-        <FavoriteMoods />
-        <SelectFromMoodCollection />
-        <div>
-          <SelectedMood />
-          <MoodEntry />
-          <SubmitMood />
+      <main className="max-w-120 flex w-full grow flex-col items-center gap-4 pt-4">
+        Add mood
+        <div className="flex w-full grow flex-col items-center justify-center gap-4">
+          {/* day overview (TODO) */}
+
+          {/* favorite moods */}
+          <div className="flex gap-4">
+            {favoriteMoods.map((m) => (
+              <MoodSwatch
+                key={m.id}
+                size="single-line-height"
+                color={m.color}
+                imgSrc={m.imagePath}
+                onClick={() => {
+                  setMood(m)
+                }}
+              />
+            ))}
+            {/* select from mood collection */}
+            <Button
+              variant="primary"
+              aria-label="view full mood collection"
+              onPress={pickFromMoodCollection}
+            >
+              <More />
+            </Button>
+          </div>
+
+          {/* submission row */}
+          <div className="mt-single-line-height flex h-single-line-height items-end gap-4">
+            <MoodSwatch
+              size="single-line-height"
+              color={mood?.color}
+              imgSrc={mood?.imagePath}
+              onClick={
+                mood
+                  ? () => {
+                      setMood(null)
+                    }
+                  : undefined
+              }
+            />
+            <TextField label="entry" onChange={setDescription} />
+            <Button
+              variant="primary"
+              aria-label="submit"
+              isDisabled={mood === null}
+              isPending={isSubmitting}
+              onPress={() => {
+                void addMoodEntry()
+              }}
+            >
+              <Send />
+            </Button>
+          </div>
         </div>
       </main>
-      <Navbar active="add" />
+      <MainNavBar />
     </>
   )
+}
+
+function useLocationState(): State | null {
+  const location = useLocation()
+  // @ts-expect-error for whatever reason the type signature has the key `_state`, but it's supposed to be `state`
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const state = location.state
+  const resolvedState = useMemo(
+    () => (validateState(state) ? state : null),
+    [state],
+  )
+  return resolvedState
+}
+
+function validateState(state: unknown): state is State {
+  if (!state) {
+    return false
+  }
+  if (typeof state !== 'object') {
+    return false
+  }
+
+  const { selectedMood } = state as Record<string, unknown>
+  if (!selectedMood) {
+    return false
+  }
+  if (typeof selectedMood !== 'object') {
+    return false
+  }
+
+  // we'll just assume it's valid otherwise
+  return true
 }
