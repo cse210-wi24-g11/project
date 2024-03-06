b9e50e37f5c17aeef64ce0c4b3c80f651a619d0b
william duan <38791932+duanwilliam@users.noreply.github.com>
2024-03-05 21:27:43 -0800

    refactor/feat: use variables for routes; and add add mood page to routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { DbProvider } from './context/db.tsx'
 import { ThemeProvider } from './context/theme.tsx'
+import { AddMood } from './pages/add-mood.tsx'
 import { DaySummary } from './pages/day-summary.tsx'
 import { WeekSummary } from './pages/week-summary.tsx'
 import { MonthSummary } from './pages/month-summary.tsx'
@@ -11,6 +12,14 @@ import { UpdateMood } from './pages/update-mood.tsx'
 import { Settings } from './pages/setting.tsx'
 import { DbTest } from './pages/db-test.tsx'
 import { ResetIndexedDb } from './components/reset-db/reset-db.tsx'
+import {
+  ADD_MOOD_ROUTE,
+  DAY_SUMMARY_ROUTE,
+  MONTH_SUMMARY_ROUTE,
+  SETTINGS_ROUTE,
+  UPDATE_MOOD_ROUTE,
+  WEEK_SUMMARY_ROUTE,
+} from './routes.ts'
 
 import './index.css'
 
@@ -20,20 +29,21 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ThemeProvider>
         <DbProvider>
           <Routes>
+            <Route path={ADD_MOOD_ROUTE} element={<AddMood />} />
+            <Route path={SETTINGS_ROUTE} element={<Settings />} />
             <Route
-              path="/DaySummary"
+              path={DAY_SUMMARY_ROUTE}
               element={<DaySummary summaryNavBarItem={'Day'} />}
             />
-            <Route path="/UpdateMood" element={<UpdateMood />} />
             <Route
-              path="/WeekSummary"
+              path={WEEK_SUMMARY_ROUTE}
               element={<WeekSummary summaryNavBarItem={'Week'} />}
             />
             <Route
-              path="/MonthSummary"
+              path={MONTH_SUMMARY_ROUTE}
               element={<MonthSummary summaryNavBarItem={'Month'} />}
             />
-            <Route path="/Settings" element={<Settings />} />
+            <Route path={UPDATE_MOOD_ROUTE} element={<UpdateMood />} />
             <Route path="/DbTest" element={<DbTest />} />
           </Routes>
         </DbProvider>
